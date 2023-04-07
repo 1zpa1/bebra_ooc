28a313b665e43efa1d79a3765bcd5070f2c7e0a3
Pavel <pzajtsev@yandex.ru>
2023-04-07 18:25:48 +0400

    Initial commit

---
+++ src/main/java/com/bebra_ooc/BebraOocApplication.java
@@ -0,0 +1,13 @@
+package com.bebra_ooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebraOocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BebraOocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bebra_ooc/BebraOocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebra_ooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebraOocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
